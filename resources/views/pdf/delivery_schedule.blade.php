1c2127d83472c2744514108e948026d4b3c43fd7
cedriclegarda97 <cedric.legarda97@gmail.com>
2022-02-13 13:03:23 +0800

    fixed titles in pdf

--- resources/views/pdf/delivery_schedule.blade.php
+++ resources/views/pdf/delivery_schedule.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Yearly Sales</title>
+	<title>Delivery Schedule Report</title>
 <style>
 	@import url(https://fonts.googleapis.com/css?family=Roboto:100,300,400,900,700,500,300,100);
 *{
