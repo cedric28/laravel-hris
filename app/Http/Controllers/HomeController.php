9eb3b4a3f2d176ccc8f58f748645d70eced91533
Cedric Nathan Legarda <cedric_legarda28@yahoo.com>
2024-04-02 10:23:29 +0800

    fix: remove unnecessary code

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,9 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
-use App\Supplier;
-use App\Inventory;
-use App\Sale;
 use Carbon\Carbon;
 
 class HomeController extends Controller
@@ -31,31 +28,7 @@ class HomeController extends Controller
         $now = Carbon::now();
         $yearNow =  $now->year;
 
-        $sales = new Sale();
-        $salesPerYear = $sales->selectRaw('year(created_at) as year, SUM(total_amount_due) as total_sales')
-            ->groupBy('year')
-            ->orderBy('year', 'asc')
-            ->get();
-        $sales = $sales->whereYear('created_at', '>=', $yearNow)->whereYear('created_at', '<=', $yearNow);
-        $totalAmountDue = $sales->sum('total_amount_due');
-
-        $product = Inventory::count();
-        $user = User::count();
-        $supplier = Supplier::count();
-        $salesPerMonth = $sales->selectRaw('month(created_at) as month, SUM(total_amount_due) as total_sales')
-            ->groupBy('month')
-            ->orderBy('month', 'asc')
-            ->get();
-
         $this->authorize("isAdmin");
-        return view('home', [
-            "user" => $user,
-            "product" => $product,
-            "supplier" => $supplier,
-            'totalAmountDue' => $totalAmountDue,
-            'year' => $yearNow,
-            'salesPerYear' => $salesPerYear,
-            'salesPerMonth' => $salesPerMonth
-        ]);
+        return view('home');
     }
 }
