44c13cb76a6a796817487931a8a2c0c2b8610025
Cedric Nathan Legarda <cedric_legarda28@yahoo.com>
2024-05-12 06:04:46 +0800

    fix: add controller for OverTime

---
+++ app/Http/Controllers/OverTime/OverTimeFetchController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\OverTime;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class OverTimeFetchController extends Controller
+{
+    //
+}
