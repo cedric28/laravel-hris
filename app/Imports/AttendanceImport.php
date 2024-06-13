b0e59b7931694992fe939752991a93919ade2a46
Cedric Nathan Legarda <cedric_legarda28@yahoo.com>
2024-06-13 20:56:52 +0800

    fix: excel skip empty row

--- app/Imports/AttendanceImport.php
+++ app/Imports/AttendanceImport.php
@@ -5,9 +5,10 @@ namespace App\Imports;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\SkipsEmptyRows;
 use Carbon\Carbon;
 
-class AttendanceImport implements ToCollection, WithHeadingRow {
+class AttendanceImport implements ToCollection, WithHeadingRow, SkipsEmptyRows {
 
     public $data;
 
